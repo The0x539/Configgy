2aebcea11c443377c62a8524190d9e53bff1b273
Hydraxous <robo-trey@outlook.com>
2024-02-22 12:54:27 -0800

    persistent type with no ui

---
+++ Configgy/Data/ConfiggyPersistent.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Configgy
+{
+    /// <summary>
+    /// Serializes values without a UI element
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ConfiggyPersistent<T> : ConfigValueElement<T> where T : IConfigElement
+    {
+        public ConfiggyPersistent(T defaultValue) : base(defaultValue) {}
+        protected override void BuildElementCore(ConfiggableAttribute descriptor, RectTransform rect) {}
+        protected override void RefreshElementValueCore() {}
+    }
+}
